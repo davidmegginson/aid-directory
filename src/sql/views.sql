8fbaa90669ded9ba42d4932ea03695b37355b4ad
David Megginson <david.megginson@gmail.com>
2025-05-27 13:21:51 -0400

    Started docker Flask implementation.

--- src/sql/views.sql
+++ src/sql/views.sql
@@ -26,13 +26,16 @@ SELECT OA.*,
   A.source_ref AS source_ref, SR.code AS source_code, SR.name AS source_name,
   S.code AS sector_code, S.name AS sector_name, S.vocabulary_ref AS sector_vocabulary_ref,
   V.code AS sector_vocabulary_code, V.name AS sector_vocabulary_name,
-  ORL.code AS org_ref_code, ORL.name AS org_ref_name
+  ORL.code AS org_role_code, ORL.name AS org_role_name,
+  ORG.code AS norm_org_code, ORG.name AS norm_org_name, ORG.type AS norm_org_type_code, ORGT.name AS norm_org_type_name
 FROM OrgActivities OA
 JOIN Activities A ON OA.activity_ref=A.id
 JOIN OrgInstances OI ON OA.org_instance_ref=OI.id
-JOIN OrgTypes OT ON OI.type=OT.id
 JOIN Sources SR ON A.source_ref=SR.id
 JOIN Sectors S ON OA.sector_ref=S.id
 JOIN SectorVocabularies V ON S.vocabulary_ref=V.id
-JOIN OrgRoles ORL ON OA.org_role_ref=ORL.id;
+JOIN OrgRoles ORL ON OA.org_role_ref=ORL.id
+LEFT JOIN OrgTypes OT ON OI.type=OT.id
+LEFT JOIN Orgs ORG ON OA.org_ref=ORG.id
+LEFT JOIN OrgTypes ORGT on ORG.type=ORGT.id;
 
